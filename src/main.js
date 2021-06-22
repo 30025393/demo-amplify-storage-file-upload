679b8562032c6fcdbddd9224a31080afef7e296b
30025393 <3002593@tafe.wa.edu.ay>
2021-06-22 15:19:40 +0800

    Add more libarys to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import App from './App.svelte';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
+import Auth from '@aws-amplify/auth';
+import Amplify from '@aws-amplify/core';
 Amplify.configure(awsconfig);
 
 const app = new App({
